c402baaa2136032fb7e334357ad84b718d859cdf
Oswaldo-kvallo <youngfrenk3@gmail.com>
2026-03-02 15:26:06 -0600

    Segun le movi a lo que es el navbar, y le di estilo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
-function Navbar(){
-    return (
-        <nav>
-            <h3>Oswaldo Cruz Lopez</h3>
-        </nav>
-    ); 
+import { NavLink } from "react-router-dom";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="logo">Oswaldo Cruz Lopez</h1>
+
+      <div className="links">
+        <NavLink to="/" end>Home</NavLink>
+        <NavLink to="/portfolio">Portfolio</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
