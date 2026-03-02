4546ee58a1554c9f7a40ab54b1811119f7e7bc58
Oswaldo-kvallo <youngfrenk3@gmail.com>
2026-03-02 16:02:03 -0600

    Agregue mas fots para el portfolio

--- src/data/photos.js
+++ src/data/photos.js
@@ -8,5 +8,20 @@ export const photos = [
     id: 2,
     title: "Celebración",
     image: "/images/futbol/gavi.webp"
+  },
+  {
+    id: 3,
+    title: "foto de equipo",
+    image: "/images/futbol/bernal.webp"
+  },
+  {
+    id: 4,
+    title: "Saque inicial",
+    image: "/images/futbol/dejong.webp"
+  },
+  {
+    id: 5,
+    title: "Fuera de juego",
+    image: "/images/futbol/lamine.webp"
   }
 ];
