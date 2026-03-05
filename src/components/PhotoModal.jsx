af421696ac5b494e7945cf0d1928ed8c8a6422c9
Oswaldo-kvallo <youngfrenk3@gmail.com>
2026-03-05 15:18:39 -0600

    Ahora cunado se selecciona una foto de portfolio si se ve como un modal emergente

--- src/components/PhotoModal.jsx
+++ src/components/PhotoModal.jsx
@@ -1,15 +1,21 @@
-function PhotoModal({photo, onClose}) {
-    if (!photo) return null
+import { createPortal } from "react-dom";
 
-    return (
-        <div className="modal-overplay" onClick={onClose}>
-            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            <img src={photo.image} alt={photo.title} />
-            <h2>{photo.title}</h2>
-            <button onClick={onClose}>Cerrar</button>
-            </div>
-        </div>
-    )
+function PhotoModal({ photo, onClose }) {
+  if (!photo) return null;
+
+  return createPortal(
+    <div className="modal-overlay" onClick={onClose}>
+      <div 
+        className="modal-content"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <img src={photo.image} alt={photo.title} />
+        <h2>{photo.title}</h2>
+        <button onClick={onClose}>Cerrar</button>
+      </div>
+    </div>,
+    document.body
+  );
 }
 
 export default PhotoModal;
